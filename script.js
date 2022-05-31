4698a67be912942d4b169304a0661ec696215e9e
PedroSantana <phsantana99@gmail.com>
2022-05-31 15:07:43 -0300

    requisito 10 deu certo, falta apenas finalizar com o alerta

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 /* eslint-disable no-use-before-define */
 // REQUISITO 1 (TITULO)
 const { body } = document;
@@ -29,20 +28,21 @@ const cor4 = document.createElement('div');
 cor4.className = 'color';
 paletaDeCores.appendChild(cor4);
 
-const borracha = document.createElement('div');
-borracha.className = 'color';
-borracha.style.backgroundColor = 'white';
-paletaDeCores.appendChild(borracha);
+// const borracha = document.createElement('div');
+// borracha.className = 'color';
+// borracha.id = 'borracha';
+// borracha.style.backgroundColor = 'rgb(254, 255, 255)';
+// paletaDeCores.appendChild(borracha);
 
 // REQUISITO 9 (BOTÃO LIMPAR CORES)
 const newSection = document.createElement('section');
 newSection.id = 'sec-button';
 body.appendChild(newSection);
 
-const clearButton = document.createElement('button');
-clearButton.id = 'clear-board';
-clearButton.innerText = 'Limpar';
-newSection.appendChild(clearButton);
+const cleanButton = document.createElement('button');
+cleanButton.id = 'clear-board';
+cleanButton.innerText = 'Limpar';
+newSection.appendChild(cleanButton);
 
 function resetColors() {
   for (let n = 0; n < pixels.length; n += 1) {
@@ -50,7 +50,7 @@ function resetColors() {
     pixels.style.backgroundColor = 'white';
   }
 }
-clearButton.addEventListener('click', resetColors);
+cleanButton.addEventListener('click', resetColors);
 
 // REQUISITO 4 E 5(QUADRO 5X5)
 const quadro = document.createElement('section');
@@ -71,7 +71,6 @@ function criarQuadroPixels(n) {
 criarQuadroPixels(5);
 
 // REQUISITO 7 (ADICIONAR CLASSE SELECTED)
-// eslint-disable-next-line complexity
 function removeClasses() {
   if (cor1.classList.contains('selected')) {
     cor1.classList.remove('selected');
@@ -85,9 +84,9 @@ function removeClasses() {
   if (cor4.classList.contains('selected')) {
     cor4.classList.remove('selected');
   }
-  if (borracha.classList.contains('selected')) {
-    borracha.classList.remove('selected');
-  }
+  // if (borracha.classList.contains('selected')) {
+  //   borracha.classList.remove('selected');
+  // }
 }
 
 function adicionaClasse1() {
@@ -110,36 +109,34 @@ function adicionaClasse4() {
   cor4.classList.add('selected');
 }
 
-function adicionaClasse5() {
-  removeClasses();
-  borracha.classList.add('selected');
-}
+// function adicionaClasse5() {
+//   removeClasses();
+//   borracha.classList.add('selected');
+// }
 
 cor1.addEventListener('click', adicionaClasse1);
 cor2.addEventListener('click', adicionaClasse2);
 cor3.addEventListener('click', adicionaClasse3);
 cor4.addEventListener('click', adicionaClasse4);
-borracha.addEventListener('click', adicionaClasse5);
+// borracha.addEventListener('click', adicionaClasse5);
 
 // REQUISITO 8 (PINTAR PIXELS)
 function pintarPixel(eventoDeOrigem) {
   const pixel = eventoDeOrigem.target;
-  console.log(eventoDeOrigem.target);
   const selectedColor = document.querySelector('.selected').style.backgroundColor;
   pixel.style.backgroundColor = selectedColor;
 }
 
-const pixels = document.querySelectorAll('.pixel');
-pixels.forEach((item) => {
-  item.addEventListener('click', pintarPixel);
-});
+// pixels.forEach((item) => {
+//   item.addEventListener('click', pintarPixel);
+// });
 
 // REQUISITO 10 [INCOMPLETO]
 const input = document.createElement('input');
 input.id = 'board-size';
 input.type = 'number';
 input.placeholder = 'Tamanho do lado';
-input.min = '5';
+input.min = '1';
 input.max = '50';
 newSection.appendChild(input);
 
@@ -151,26 +148,13 @@ newSection.appendChild(buttonVQV);
 function botaoVQV() {
   quadro.innerHTML = '';
   criarQuadroPixels(input.value);
-  console.log(input.value);
 }
 
 buttonVQV.addEventListener('click', botaoVQV);
-
-// REQUISITO 11 [INCOMPLETO]
-// function sizeLimit() {
-//   const number = input.value;
-//   if (number < 1) {
-//     alert('Board Inválido!');
-//   }
-//   if (number < 5) {
-//     return 5;
-//   }
-//   if (number > 50) {
-//     return 50;
-//   }
-// }
-
-// sizeLimit();
+const pixels = document.querySelectorAll('.pixel');
+quadro.addEventListener('click', (evento) => {
+  pintarPixel(evento);
+});
 
 // REQUISITO 12 (CORES ALEATÓRIAS)
 function numeroAleatorio() {
@@ -182,23 +166,9 @@ function numeroAleatorio() {
 }
 function coresAleatorias() {
   const colors = document.querySelectorAll('.color');
-  for (let i = 1; i < colors.length - 1; i += 1) {
+  for (let i = 1; i < colors.length; i += 1) {
     colors[i].style.backgroundColor = numeroAleatorio();
   }
 }
 
 coresAleatorias();
-
-// // RODAPE
-// const rodape = document.createElement('footer');
-// rodape.id = 'rodape';
-// body.appendChild(rodape);
-
-// const section = document.createElement('section');
-// section.innerHTML = '<section><strong> Projeto Pixels Art - Bloco 05: JavaScript: DOM, Eventos e Web Storage</strong><br>Pedro Henrique Santana de Menezes @ Maio 2022</section>';
-// rodape.appendChild(section);
-
-// const images = document.createElement('section');
-// images.style.margin = '20px 0 0 0';
-// images.innerHTML = '<strong>Links Pessoais</strong> <br><a href="https://github.com/santanap2" target="_blank"><img src="./rsz_github.png" alt="Logo Github" class="logos"></a> <a class="logos" href="https://www.instagram.com/santanap2/" target="_blank"><img src="rsz_1instagram.png" alt="Logo Instagram" class="logos"></a> <a class="button" href="https://www.linkedin.com/in/pedro-santana-895445238/" target="_blank"><img src="./rsz_linkedin.png" alt="Lobo LinkedIn" class="logos"></a>';
-// rodape.appendChild(images);
